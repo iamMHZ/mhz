b6798fcd6767f9a576fb4f7a486b8e4c057fd99d
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-07-01 11:32:35 +0430

    updated setup files

--- setup.py
+++ setup.py
@@ -3,23 +3,23 @@ from distutils.core import setup
 setup(
     name='mhz',
     packages=['mhz'],
-    version='1.0',
-    license='GNU',
+    version='v1.0.0',
+    license='GPLv3',
     description='Just a playground for adding a package in pypi',
     author='MHZ',
     author_email='ziyaaddini@gmail.com',
     url='https://github.com/iamMHZ/mhz',
-    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',  # I explain this later on
-    keywords=['MHZ', 'iam_mhz'],  # Keywords that define your package best
+    download_url='https://github.com/iamMHZ/mhz/archive/refs/tags/v1.0.0.tar.gz',
+    keywords=['MHZ', 'iam_mhz'],
     install_requires=[
 
     ],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: GNU License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3',
     ],
 )
