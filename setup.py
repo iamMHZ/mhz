de5fdc5e18de2caf504190950fea50ad0cc0c8d7
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-07-01 10:58:30 +0430

    added setup files

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name='mhz',
+    packages=['mhz'],
+    version='1.0',
+    license='GNU',
+    description='Just a playground for adding a package in pypi',
+    author='MHZ',
+    author_email='ziyaaddini@gmail.com',
+    url='https://github.com/iamMHZ/mhz',
+    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',  # I explain this later on
+    keywords=['MHZ', 'iam_mhz'],  # Keywords that define your package best
+    install_requires=[
+
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU License',
+        'Programming Language :: Python :: 3',
+    ],
+)
